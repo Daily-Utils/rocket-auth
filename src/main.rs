52e966e98aea093d3da929e70ce0865e0684d99f
siddh34 <ssiddharthsutar@gmail.com>
2024-10-12 01:13:57 +0530

    feat: justfile

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
-mod utils;
+mod controllers;
 mod models;
 mod schema;
-mod controllers;
+mod utils;
 
-use crate::utils::connect_sql::establish_connection;
-use crate::controllers::tenant:: refresh_tenant_key::refresh_tenant;
 use crate::controllers::client::create_client::create_client;
+use crate::controllers::tenant::refresh_tenant_key::refresh_tenant;
+use crate::utils::connect_sql::establish_connection;
 use controllers::tenant::create_tenant::create_tenant;
-use rocket::{get, routes, launch};
+use rocket::{get, launch, routes};
 
 #[get("/")]
 fn hello() -> String {
-    format!("Server is running!")
+    format!("Server is runningsss!!!!!")
 }
 
 #[launch]
 async fn rocket() -> _ {
     let _ = establish_connection().await.unwrap();
-    
+
     let rocker_build: rocket::Rocket<rocket::Build> = rocket::build();
 
     rocker_build
