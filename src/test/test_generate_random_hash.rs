c5198c419c3fccb187fda2190df31c121c2f370b
siddh34 <ssiddharthsutar@gmail.com>
2024-10-31 19:11:41 +0530

    feat: unit tests

---
+++ src/test/test_generate_random_hash.rs
@@ -0,0 +1,10 @@
+#[cfg(test)]
+mod test {
+    use crate::utils::generate_random_hash::generate_random_hash_function;
+
+    #[test]
+    fn test_generate_random_hash() {
+        let hash = generate_random_hash_function(64);
+        assert_eq!(hash.len(), 64);
+    }
+}
---
+++ src/test/test_generate_short_hash.rs
@@ -0,0 +1,24 @@
+#[cfg(test)]
+mod test {
+    use crate::utils::generate_short_hash::{encrypt, decrypt};
+
+    #[test]
+    fn test_encrypt_decrypt() {
+        let key = "12345678901234567890123456789012";
+        let data = "Hello, World!";
+        let encrypted_data = encrypt(data, key, 16);
+        let decrypted_data = decrypt(&encrypted_data, key).unwrap();
+        assert_eq!(data, decrypted_data);
+    }
+
+    #[test]
+    #[should_panic(expected = "decryption failed")]
+    fn test_encrypt_decrypt_with_different_key() {
+        let key = "12345678901234567890123456789012";
+        let data = "Hello, World!";
+        let encrypted_data = encrypt(data, key, 16);
+        let key = "12345678901234567890123456789013";
+        let decrypted_data = decrypt(&encrypted_data, key).expect("decryption failed");
+        assert_ne!(data, decrypted_data);
+    }
+}
